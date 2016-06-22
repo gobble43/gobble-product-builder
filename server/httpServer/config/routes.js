72ab3fb3b6b5f601bdef0c3c0685f608c717a4a9
Leo Adelstein <leoadelstein@gmail.com>
2016-06-22 15:32:29 -0700

    added http response status codes

--- server/httpServer/config/routes.js
+++ server/httpServer/config/routes.js
@@ -1,11 +1,17 @@
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    res.end('hello');
+    res.end('Hello World!');
   });
   app.post('/api/product', (req, res) => {
     console.log('form body', req.body);
-    process.send(req.body);
-    res.end();
+    if (typeof req.body.upc === 'string') {
+      process.send(req.body);
+      res.statusCode = 201;
+      res.end();
+    } else {
+      res.statusCode = 400;
+      res.end();
+    }
   });
 };
