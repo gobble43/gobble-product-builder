9597732d4c454c0aeca73c8803043d165421d23a
Leo Adelstein <leoadelstein@gmail.com>
2016-06-22 14:57:07 -0700

    updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,11 @@ module.exports = {
     "installedESLint": true,
     "plugins": [
         "react"
-    ]
+    ],
+    "rules": {
+    "no-console": 0,  // allow use of console (off by default in the node environment)
+    "no-undef": 0, // allow undefined 'describe' function from mocha
+    "global-require": 0, // needed for cluster architecture
+    "no-underscore-dangle": 0, // needed for parsing an api response
+  }
 };
