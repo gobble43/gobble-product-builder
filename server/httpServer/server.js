2e7bcb474041427c27b0b1e85d73c3179e92aeed
Leo Adelstein <leoadelstein@gmail.com>
2016-06-22 15:03:17 -0700

    server uses env properties now

--- server/httpServer/server.js
+++ server/httpServer/server.js
@@ -1,3 +1,11 @@
+// Load environment variables
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: './env/development.env' });
+} else if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: './env/production.env' });
+}
+
 const express = require('express');
 
 const app = express();
@@ -5,6 +13,6 @@ const app = express();
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-const server = app.listen(process.env.PORT || 3001, () => {
-  console.log('listening on port: ', server.address().port);
+app.listen(process.env.PORT, () => {
+  console.log(`${process.env.APP_NAME} is listening on port ${process.env.PORT}.`);
 });
